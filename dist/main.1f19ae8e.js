95924b88a9a6a629fe0809dde1d9338ea91f52c3
Eric <840772940@qq.com>
2019-11-25 22:51:40 +0800

    更新

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -191,7 +191,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13660" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
